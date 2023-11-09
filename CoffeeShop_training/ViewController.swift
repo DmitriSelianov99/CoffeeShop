255dbc6de0be7313804f048dfa9ae65408499e19
DmitriSelianov99 <96311145+DmitriSelianov99@users.noreply.github.com>
2023-11-09 12:00:59 +0500

    Initial Commit

---
+++ CoffeeShop_training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoffeeShop_training
+//
+//  Created by Дмитрий Сельянов on 09.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
